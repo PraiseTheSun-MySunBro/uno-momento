269b8c4f91c6e005179c0528663129db6f498646
Denis <34522773+spyroks@users.noreply.github.com>
2018-02-27 00:20:15 +0200

    Revert "Revert "created models, repository and service classes""

---
+++ src/main/java/ee/ttu/unomomento/models/Faculty.java
@@ -0,0 +1,23 @@
+package ee.ttu.unomomento.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@Entity
+public class Faculty {
+
+    @Id
+    private Short facultyCode;
+
+    private String eeName;
+    private String enName;
+}
