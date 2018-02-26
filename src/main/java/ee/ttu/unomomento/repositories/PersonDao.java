2142fcc74205a6ff64855e48f2ef10cb7d5a12c1
Denis <34522773+spyroks@users.noreply.github.com>
2018-02-26 23:13:42 +0200

    Revert "created repositories for each model"

--- src/main/java/ee/ttu/unomomento/repositories/PersonDao.java
+++ src/main/java/ee/ttu/unomomento/repositories/PersonDao.java
@@ -2,11 +2,8 @@ package ee.ttu.unomomento.repositories;
 
 import ee.ttu.unomomento.models.Person;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface PersonDao extends JpaRepository<Person, Long> {
-
-    Person findByPersonId(@Param("person_id") Long personId);
+public interface PersonDao extends JpaRepository <Person, Long> {
 }
