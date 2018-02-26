9ff2d9bd617166d82eddf74402e6290a57d716e5
Denis <34522773+spyroks@users.noreply.github.com>
2018-02-27 00:10:47 +0200

    Revert "created models, repository and service classes"

--- src/main/java/ee/ttu/unomomento/services/PersonService.java
+++ src/main/java/ee/ttu/unomomento/services/PersonService.java
@@ -2,6 +2,7 @@ package ee.ttu.unomomento.services;
 
 import ee.ttu.unomomento.models.Person;
 import ee.ttu.unomomento.repositories.PersonDao;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -11,7 +12,6 @@ import java.util.List;
 @Transactional
 @Service
 public class PersonService {
-
     @Autowired
     private PersonDao personDao;
 
