4eafefab77d4b428de5ca6a29a9b21c2ac5373bb
Solaire of Astora <36454835+PraiseTheSun-MySunBro@users.noreply.github.com>
2018-02-27 19:08:54 +0200

    refactored file structure, removed thymeleaf, removed travis CI email notification, added json config, renamed local config (#13)

--- src/main/java/ee/ttu/unomomento/controllers/HomeController.java
+++ src/main/java/ee/ttu/unomomento/controllers/HomeController.java
@@ -1,24 +1,30 @@
 package ee.ttu.unomomento.controllers;
 
-import ee.ttu.unomomento.configurations.Config;
+import com.google.gson.Gson;
+import ee.ttu.unomomento.configurations.LocaleConfig;
 import ee.ttu.unomomento.services.PersonService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.util.ResourceBundle;
+
 @Slf4j
 @Controller
 public class HomeController {
     @Autowired
-    private Config config;
+    private ResourceBundle bundle;
+
+    @Autowired
+    private Gson gson;
 
     @Autowired
     private PersonService personService;
 
     @GetMapping(value = "/")
     public String index() {
-        System.out.println(personService.getPersons());
+        log.info("==========GET PERSONS==========\n{}", gson.toJson(personService.getPersons()));
         return "index";
     }
 }
