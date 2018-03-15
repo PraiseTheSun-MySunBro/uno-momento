05d9e14a1177436f4b04d1b87b902825cafd0b69
Chapaev <erpris@ttu.ee>
2018-03-15 19:09:00 +0200

    added login component

---
+++ frontend/src/components/Login.vue
@@ -0,0 +1,53 @@
+<template>
+  <b-container fluid>
+    <b-container class="auth">
+      <b-form @submit.prevent="onSubmit">
+        <b-card bg-variant="light">
+          <b-form-group horizontal
+                        label="Email:"
+                        label-class="text-sm"
+                        label-for="auth_email">
+            <b-form-input id="auth_email" type="email" v-model="credentials.email"></b-form-input>
+          </b-form-group>
+          <b-form-group horizontal
+                        label="Password:"
+                        label-class="text-sm"
+                        label-for="auth_password">
+            <b-form-input id="auth_password" type="password" v-model="credentials.password"></b-form-input>
+          </b-form-group>
+          <b-form-group>
+            <b-button variant="primary">Login</b-button>
+          </b-form-group>
+        </b-card>
+      </b-form>
+    </b-container>
+  </b-container>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      credentials: {
+        email: '',
+        password: ''
+      }
+    }
+  },
+  methods: {
+    onSubmit () {
+      // let app = this
+      // this.$auth.login({
+        
+      // })
+    }
+  }
+}
+</script>
+
+<style>
+  .auth {
+    margin-top: 150px;
+    max-width: 600px;
+  }
+</style>
--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import Router from 'vue-router'
 
 import Home from '@/components/Home'
+import Login from '@/components/Login'
 
 Vue.use(Router)
 
@@ -11,6 +12,11 @@ export default new Router({
       path: '/',
       name: 'Home',
       component: Home
+    },
+    {
+      path: '/login',
+      name: 'Login',
+      component: Login
     }
   ]
 })
