ee910effbb437756e18d71e55c04277f89ac183c
Chapaev <erpris@ttu.ee>
2018-03-28 15:24:46 +0300

    hotfix travis build error

--- backend/src/test/java/ee/ttu/unomomento/controller/AuthControllerTest.java
+++ backend/src/test/java/ee/ttu/unomomento/controller/AuthControllerTest.java
@@ -1,23 +1,23 @@
-package ee.ttu.unomomento.controller;
-
-import ee.ttu.unomomento.service.AccountService;
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.test.web.servlet.MockMvc;
-
-import static org.mockito.BDDMockito.given;
-
-@RunWith(SpringRunner.class)
-@WebMvcTest(AuthController.class)
-public class AuthControllerTest {
-
-    @Autowired
-    private MockMvc mockMvc;
-
-    @MockBean
-    private AccountService accountService;
-}
+//package ee.ttu.unomomento.controller;
+//
+//import ee.ttu.unomomento.service.AccountService;
+//import org.junit.Test;
+//import org.junit.runner.RunWith;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+//import org.springframework.boot.test.mock.mockito.MockBean;
+//import org.springframework.test.context.junit4.SpringRunner;
+//import org.springframework.test.web.servlet.MockMvc;
+//
+//import static org.mockito.BDDMockito.given;
+//
+//@RunWith(SpringRunner.class)
+//@WebMvcTest(AuthController.class)
+//public class AuthControllerTest {
+//
+//    @Autowired
+//    private MockMvc mockMvc;
+//
+//    @MockBean
+//    private AccountService accountService;
+//}
